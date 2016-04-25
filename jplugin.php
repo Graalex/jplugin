d89537b3807f0e4d10b678f3fb7550c4608cea46
Gralex <aleksandr.grigorchuk@gmail.com>
2016-04-25 08:44:27 +0300

    Add language support.

--- jplugin.php
+++ jplugin.php
@@ -1,18 +1,48 @@
 <?php
-    define('_JEXEC') or die;
+/**
+ * pagage   Joomla.Plugin
+ * subpacage JPlugin plugin
+ * copyright (c) 2016. Grigorchuk Aleksandr
+ * license MIT
+ */
 
-    class plgJPlugin extends JPlugin
+define('_JEXEC') or die;
+
+/**
+ * Class plgJPlugin
+*/
+class plgJPlugin extends JPlugin
+{
+    protected $autoloadLanguage = true;
+
+    /**
+     * Обрабатывает событие onContentPrepare.
+     *
+     * @param   string  $context    Контекст в котором начинает обрабатіваться контент
+     * @param   mixed   $article    Статья
+     * @param   mixed $params   Дополнительные параметры переданные в плагин
+     * @param   integer $page   Необязательный номер страницы. По умолчанию 0
+     *
+     * @return bool True если обработка прошла успешно
+    */
+    public function onContentPrepare($context, &$article, &$params, $page)
     {
-        protected $autoloadLanguage = true;
-
-        public function onContentPrepare($context, &$article, &$params, $page)
-        {
-            return true;
-        }
-
-        public function onContentAfterTitle($context, &$article, &$params, $limitstatrt)
-        {
-            return '';
-        }
+        //TODO: Добавить логику обработки события.
+        return true;
     }
-?>
+
+    /**
+     * Обрабатывает событие onContentAfterTitle
+     *
+    * @param string  $context    Контекст в котором начинает обрабатіваться контент
+     * @param   mixed   $article    Статья
+     * @param   mixed $params   Дополнительные параметры переданные в плагин
+     * @param   integer $limitstatrt    Номер страницы с которой добавляется контент
+     * @return  string Добавляемый контент после заголовка страницы
+    */
+    public function onContentAfterTitle($context, &$article, &$params, $limitstatrt)
+    {
+        //TODO: Добавить логику обработки события.
+        return '';
+    }
+}
